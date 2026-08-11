ee022a9d6c57d8a6590d44bf549998399270d92c
Xinyue (Margaret) Zhang <xinyuezhang2025@u.northwestern.edu>
2026-08-11 14:34:04 -0500

    Add: two-integer-sum - submission-18

---
+++ Data Structures & Algorithms/two-integer-sum/submission-18.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        count = {} # number index (int int)
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in count:
+                return [count[diff], i]
+            count[n] = i
